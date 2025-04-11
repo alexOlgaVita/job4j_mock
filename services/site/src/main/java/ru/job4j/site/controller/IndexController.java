392630c520ce19c57926429b6fbf0d002bbb2001
olga.ilyina <olga.ilyina@axenix.pro>
2025-04-11 23:55:28 +0300

    4. Микросервисные системы [#338434]

--- services/site/src/main/java/ru/job4j/site/controller/IndexController.java
+++ services/site/src/main/java/ru/job4j/site/controller/IndexController.java
@@ -6,12 +6,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.job4j.site.service.AuthService;
-import ru.job4j.site.service.CategoriesService;
-import ru.job4j.site.service.InterviewsService;
-import ru.job4j.site.service.NotificationService;
+import ru.job4j.site.dto.ProfileDTO;
+import ru.job4j.site.service.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static ru.job4j.site.controller.RequestResponseTools.getToken;
 
@@ -19,16 +20,17 @@ import static ru.job4j.site.controller.RequestResponseTools.getToken;
 @AllArgsConstructor
 @Slf4j
 public class IndexController {
+    private static final int PAGE = 0;
+    private static final int SIZE = 20;
     private final CategoriesService categoriesService;
     private final InterviewsService interviewsService;
     private final AuthService authService;
     private final NotificationService notifications;
+    private final ProfilesService profilesService;
 
     @GetMapping({"/", "index"})
     public String getIndexPage(Model model, HttpServletRequest req) throws JsonProcessingException {
-        RequestResponseTools.addAttrBreadcrumbs(model,
-                "Главная", "/"
-        );
+        RequestResponseTools.addAttrBreadcrumbs(model, "Главная", "/");
         try {
             model.addAttribute("categories", categoriesService.getMostPopular());
             var token = getToken(req);
@@ -41,6 +43,16 @@ public class IndexController {
         } catch (Exception e) {
             log.error("Remote application not responding. Error: {}. {}, ", e.getCause(), e.getMessage());
         }
+
+        var interviewsPage = interviewsService.getAll(null, PAGE, SIZE);
+        Set<ProfileDTO> userList = (interviewsPage != null) ? interviewsPage.toList().stream()
+                .map(x -> profilesService.getProfileById(x.getSubmitterId()))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.toSet()) : Set.of();
+
+        model.addAttribute("users", userList);
+
         model.addAttribute("new_interviews", interviewsService.getByType(1));
         return "index";
     }
